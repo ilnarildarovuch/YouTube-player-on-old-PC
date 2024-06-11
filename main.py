9465ffbaa13cdfe67b9ba127fc7727d178e3f06b
ilnarildarovuch <ilnarildarovuch@gmail.ru>
2024-06-11 20:25:30 +0500

    Eng

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from youtubesearchpython import VideosSearch
 import os
 from pytube import YouTube
 
+os.chdir("D:/Program Files/VideoLAN/VLC/")
+
 while True:
-    os.chdir("D:/Program Files/VideoLAN/VLC/vid")
+    os.chdir("vid")
 
     for i in os.listdir():
         os.remove(i)
     os.chdir("..")
-    videosSearch = VideosSearch(input("Видео: "), limit=1)
+    videosSearch = VideosSearch(input("Video: "), limit=1)
     videos = videosSearch.result()
     print(videos["result"][0]["link"])
-    yt = YouTube(videos["result"][0]["link"]).streams.filter(res="360p", only_video=False, only_audio=False)
-    yt.first().download("./vid")
-    os.system('vlc "vid/' + os.listdir("./vid")[0] + '"')
-    input()
+    if input("To download? (y/n) ") == "y":
+        res = input("Resolution [360p, 480p, 720p]:")
+        if not res in ["360p", "480p", "720p"]:
+            res = "360p"
+        yt = YouTube(videos["result"][0]["link"]).streams.filter(res=res, only_video=False, only_audio=False)
+        yt.first().download("./vid")
+        os.system('vlc "vid/' + os.listdir("./vid")[0] + '"')
